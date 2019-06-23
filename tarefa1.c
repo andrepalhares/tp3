1a93cd987f8395b758a484e19383bef693a3baa4
andrepalhares <33261635+andrepalhares@users.noreply.github.com>
2019-06-23 18:14:03 -0300

    Update tarefa1.c

--- tarefa1.c
+++ tarefa1.c
@@ -1,129 +1,23 @@
-#include <stdio.h> 
+#include<stdio.h> 
 #include <stdlib.h> 
+#include <stdbool.h>
+#include <string.h>
 
 struct No {
 	int indice;
-	struct No* proximo;
+	struct No *proximo;
 };
 
 struct Lista {
-	int adjacencias;
-	int checado;
+	int indice;
 	struct No* cabeca;
 };
 
 struct Grafo {
 	int vertices;
 	int arestas;
-	struct Lista* adj;
+	struct Lista * adj;
 };
-
-struct No* cria_no(int vertice) {
-	struct No* novo_no = (struct No*) malloc (sizeof(struct No));
-	novo_no->indice = vertice;
-	novo_no->proximo = NULL;
-	return novo_no;
-}
-
-struct Grafo* cria_grafo(int V, int A) {
-	struct Grafo* grafo = (struct Grafo*) malloc(sizeof(struct Grafo));
-	grafo->vertices = V;
-	grafo->arestas = A;
-	grafo->adj = (struct Lista*) malloc(V * sizeof(struct Lista));
-	
-	for(int i=0; i < V; i++) {
-		grafo->adj[i].cabeca = NULL;
-		grafo->adj[i].checado = 0;
-	}
-	
-	return grafo;
-}
-
-void adiciona_filho(struct Grafo* grafo, int a, int b) {
-	struct No* novo_no = cria_no(b);
-	novo_no->proximo = grafo->adj[a].cabeca;
-	grafo->adj[a].cabeca = novo_no;
-	grafo->adj[a].adjacencias++;
-}
-
-void print_grafo(struct Grafo* grafo) {
-	for(int i=0; i < grafo->vertices; i++) {
-		struct No* aux = grafo->adj[i].cabeca;
-		printf("[%d] -> ", i);
-		
-		while(aux) {
-			printf("%d ", aux->indice);
-			aux = aux->proximo;
-		}
-		printf("\n");
-	}
-}
-
-int vertex_cover(struct Grafo* graph, int index) {
-	if(graph->adj[index].adjacencias == 0) {
-		printf("%d\n", graph->adj[index].adjacencias);
-		return 0;
-	}
-	
-	if(graph->adj[index].checado != 0) {
-		return graph->adj[index].checado;
-	}
-	
-	//Calcula a solução mínima quando o nó raiz faz parte da solução
-	int vc_com_raiz = 1; //o próprio nó raiz 
-	int indice_filho = -1;
-	struct No* filho = graph->adj[index].cabeca;
-	while(filho) {
-		indice_filho = filho->indice;
-		vc_com_raiz += vertex_cover(graph, indice_filho);
-		filho = filho->proximo;
-	}
-	
-	//Calcula a solução mínima quando o nó raiz NÃO faz parte da solução
-	int vc_sem_raiz = 0;
-	int indice_neto = -1;
-	struct No* neto = graph->adj[index].cabeca;
-	filho = graph->adj[index].cabeca;
-	while(filho) {
-		vc_sem_raiz++;
-		indice_filho = filho->indice;
-		neto = graph->adj[indice_filho].cabeca;
-		while(neto) {
-			indice_neto = neto->indice;
-			vc_sem_raiz += vertex_cover(graph, indice_neto);
-			neto = neto->proximo;
-		}
-		filho = filho->proximo;
-	}
-	
-	//printf("\n\ncom %d sem %d\n\n", vc_com_raiz, vc_sem_raiz);
-	
-	if(vc_com_raiz < vc_sem_raiz)
-		graph->adj[index].checado = vc_com_raiz;
-	graph->adj[index].checado = vc_sem_raiz;
-	
-	//printf("\n Node raiz da vez: %d\nGrau de cobertura: %d\n", index, graph->adj[index].checado);
-	
-	return graph->adj[index].checado;	
-}
-
-void inicializa_arestas(char nome[64], struct Grafo* grafo) {
-	FILE *trilha;
-	trilha = fopen(nome, "r");
-	char line[256];
-	int i = 0;
-	int j = 0;
-	int vertice1, vertice2;
-	while (fgets(line, sizeof(line), trilha)) {
-		i++;
-		if(i <= grafo->arestas ) {
-			fscanf(trilha, "%d %d", &vertice1, &vertice2);
-			adiciona_filho(grafo, vertice1, vertice2);
-			j++;
-		}
-	}
-	fclose(trilha);
-}
 
 void inicializa_grafo(char nome[64], int * vertices, int * arestas) {
 	FILE *trilha;
@@ -139,11 +33,128 @@ void inicializa_grafo(char nome[64], int * vertices, int * arestas) {
 	fclose(trilha);
 }
 
+struct No* cria_no(int vertice) {
+	struct No* novo_no = (struct No*) malloc (sizeof(struct No));
+	novo_no->indice = vertice;
+	novo_no->proximo = NULL;
+	return novo_no;
+}
+
+void adiciona_aresta(struct Grafo* grafo, int a, int b) {
+	struct No* no_a = cria_no(b);
+	struct No* no_b = cria_no(a);
+	
+	no_a->proximo = grafo->adj[a].cabeca;
+	no_b->proximo = grafo->adj[b].cabeca;
+	
+	grafo->adj[a].cabeca = no_a;
+	grafo->adj[b].cabeca = no_b;
+}
+
+void inicializa_arestas(char nome[64], struct Grafo* grafo) {
+	FILE *trilha;
+	trilha = fopen(nome, "r");
+	char line[256];
+	int i = 0;
+	int j = 0;
+	int vertice1, vertice2;
+	while (fgets(line, sizeof(line), trilha)) {
+		i++;
+		if(i <= grafo->arestas ) {
+			fscanf(trilha, "%d %d", &vertice1, &vertice2);
+			adiciona_aresta(grafo, vertice1, vertice2);
+			j++;
+		}
+	}
+	fclose(trilha);
+}
+
+struct Grafo* cria_grafo(int V, int A) {
+	struct Grafo* grafo = (struct Grafo*) malloc(sizeof(struct Grafo));
+	grafo->vertices = V;
+	grafo->arestas = A;
+	grafo->adj = (struct Lista*) malloc(V * sizeof(struct Lista));
+	
+	for(int i=0; i < V; i++) {
+		grafo->adj[i].indice = i;
+		grafo->adj[i].cabeca = NULL;
+	}
+	
+	return grafo;
+}
+
+void print_grafo(struct Grafo* grafo) {
+	for(int i=0; i < grafo->vertices; i++) {
+		struct No* aux = grafo->adj[i].cabeca;
+		printf("[%d] -> ", i);
+		
+		while(aux) {
+			printf("%d ", aux->indice);
+			aux = aux->proximo;
+		}
+		printf("\n");
+	}
+}
+
+struct Lista retorna_vizinhos(struct Grafo* grafo, int indice) {
+	struct Lista vizinhos;
+	vizinhos = grafo->adj[indice];
+	return vizinhos;
+}
+
+int min(int x, int y) { return (x < y)? x: y; }
+
+int vertex_cover(struct Grafo* grafo, int indice, int *checado, int *valor_lista) {
+	int prox_no, filho_no, valor = 0;
+	checado[indice] = 1;
+	if(valor_lista[indice]) {
+		checado[indice] = 0;
+		return valor_lista[indice];
+	}
+	
+	int vc_com_raiz = 1;
+	struct No *no_aux = grafo->adj[indice].cabeca;
+	while(no_aux) {
+		prox_no = no_aux->indice;
+		if(checado[prox_no] == 0) {
+			vc_com_raiz += vertex_cover(grafo, prox_no, checado, valor_lista);
+		}
+		no_aux = no_aux->proximo;
+	}
+	
+	int vc_sem_raiz = 0;
+	no_aux = grafo->adj[indice].cabeca;
+	while(no_aux) {
+		prox_no = no_aux->indice;
+		if(checado[prox_no] == 0) {
+			vc_sem_raiz++;
+			struct No *seg_no = grafo->adj[prox_no].cabeca;
+			while(seg_no) {
+				filho_no = seg_no->indice;
+				if (filho_no != indice) {
+					vc_sem_raiz += valor_lista[filho_no];
+				}
+				seg_no = seg_no->proximo;
+			}
+		}
+		no_aux = no_aux->proximo;
+	}
+	
+	int resultado = min(vc_com_raiz, vc_sem_raiz);
+	valor_lista[indice] = resultado;
+	checado[indice] = 0;
+	return resultado;
+}
+
 void main(int argc, char ** argv) {
 	int vertices, arestas;
 	inicializa_grafo(argv[1], &vertices, &arestas);
+	int checado[vertices];
+	memset(checado, 0, vertices*sizeof(checado[0]));
+	int valor_lista[vertices];
+	memset(valor_lista, 0, vertices*sizeof(valor_lista[0]));
 	struct Grafo* graph = cria_grafo(vertices, arestas);
 	inicializa_arestas(argv[1], graph);
 	//print_grafo(graph);
-	printf("%d", vertex_cover(graph, 0));
+	printf("%d", vertex_cover(graph, 0, checado, valor_lista));
 }
